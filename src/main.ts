150d7ef82bc5bdf291a4321d039f035e1a2f68b7
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-08 13:51:02 +0100

    update default port

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = 8888;
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(PORT);
+  console.log('Listening on port', PORT);
 }
 bootstrap();
