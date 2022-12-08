a256f0301bbd6d6481c8b2bd4b739290d85f8e54
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-08 20:51:25 +0100

    Serve icon

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = 8888;
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/public/',
+  });
   await app.listen(PORT);
   console.log('Listening on port', PORT);
 }
