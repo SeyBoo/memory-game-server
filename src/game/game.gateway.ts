9f6824f80dc5ce1c6ee32be00685f820b23fe7f4
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-08 20:57:01 +0100

    start game

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -3,16 +3,37 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  ConnectedSocket,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Socket, Server } from 'socket.io';
+import { GameService } from './game.service';
+import { StartGameProps } from './types/game.interface';
+import { v4 as uuid } from 'uuid';
 
 @WebSocketGateway({ cors: true })
 export class GameGateway {
+  constructor(private gameService: GameService) {}
+
   @WebSocketServer()
   server: Server;
-  @SubscribeMessage('send_message')
-  listenForMessages(@MessageBody() data: string) {
-    console.log(data);
-    this.server.sockets.emit('receive_message', data);
+
+  @SubscribeMessage('start_game')
+  createRoom(
+    @MessageBody() data: StartGameProps,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const id: string = uuid();
+    client.join(id);
+    client.emit(
+      'game_started',
+      this.gameService.startGame({
+        roomId: id,
+        gridSize: data.gridSize,
+        playerCount: data.playerCount,
+        theme: data.theme,
+      }),
+    );
+  }
+
   }
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,4 +1,86 @@
 import { Injectable } from '@nestjs/common';
+import {
+  FligImageReponse,
+  FlipImageServiceProps,
+  Game,
+  GameArrayItem,
+  gridSizeI,
+  Room,
+  StartGameProps,
+  themeI,
+} from './types/game.interface';
+import GenerateShuffledImages from './utils/generateShuffledImages';
+import GenerateShuffledNumbers from './utils/generateShuffledNumbers';
 
 @Injectable()
-export class GameService {}
+export class GameService {
+  private _rooms: Room[] = [
+    {
+      cards: {
+        cardsChosen: [],
+        cardsChosenIds: [],
+        openCards: [],
+      },
+      game: {
+        gridSize: 8,
+        gridValue: [],
+        moveCounter: 0,
+        playerCount: 1,
+        theme: 'icons',
+        won: false,
+      },
+      roomId: '',
+    },
+  ];
+  public get getRooms(): Room[] {
+    return this._rooms;
+  }
+  public set setRooms(value: Room[]) {
+    this._rooms = value;
+  }
+
+  generateShuffledGridValue(
+    theme: themeI,
+    gridSize: gridSizeI,
+  ): GameArrayItem[] {
+    if (theme === 'icons') {
+      return GenerateShuffledImages(gridSize);
+    } else {
+      return GenerateShuffledNumbers(gridSize);
+    }
+  }
+
+  startGame({ gridSize, playerCount, theme, roomId }: StartGameProps): Game {
+    const newCreatedRoom: Room = {
+      cards: {
+        cardsChosen: [],
+        cardsChosenIds: [],
+        openCards: [],
+      },
+      game: {
+        gridSize,
+        gridValue: this.generateShuffledGridValue(theme, gridSize),
+        moveCounter: 0,
+        playerCount,
+        theme: 'numbers',
+        won: false,
+      },
+      roomId,
+    };
+
+    this.setRooms = [...this.getRooms, newCreatedRoom];
+
+    return newCreatedRoom.game;
+  }
+
+  async joinGame() {
+    return;
+  }
+
+  flipImage({
+    value,
+    index,
+    roomId,
+  }: FlipImageServiceProps): FligImageReponse | void {
+    return;
+}
